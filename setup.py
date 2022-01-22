45fe6eba25f372a127e9c03ccd702bcb1e9b2185
fluzzi32 <fluzzi32@Norman.lan>
2022-01-22 09:26:53 -0500

    update compose file location

--- setup.py
+++ setup.py
@@ -15,6 +15,11 @@ class bcolors:
 if not os.path.exists('log'):
         os.makedirs('log')
 
+configdir = os.path.expanduser('~') + '/.config/rdp2tcp'
+if not os.path.exists(configdir):
+        os.makedirs(configdir)
+os.popen('cp docker-compose.yml ' + configdir)
+
 build = "sudo docker build --no-cache -t gederico/rdp2tcp:v1.0.0 ."
 start = subprocess.call(build,shell=True)
 
